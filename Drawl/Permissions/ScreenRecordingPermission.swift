b9fa113cc77e54cd108a2f2cd4a516665df890f2
Ben Anderson <andersonb97@gmail.com>
2026-06-25 13:22:06 -0500

    feat: add ScreenRecordingPermission wrapper

---
+++ Drawl/Permissions/ScreenRecordingPermission.swift
@@ -0,0 +1,27 @@
+import CoreGraphics
+import AppKit
+
+public class ScreenRecordingPermission: ObservableObject {
+    @Published public var isGranted: Bool = false
+
+    public init() {
+        checkStatus()
+    }
+
+    public func checkStatus() {
+        isGranted = CGPreflightScreenCaptureAccess()
+    }
+
+    public func requestAccess() {
+        CGRequestScreenCaptureAccess()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.checkStatus()
+        }
+    }
+
+    public func openSystemSettings() {
+        if let url = URL(string: "x-apple.systempreferences:com.apple.preference.security?Privacy_ScreenCapture") {
+            NSWorkspace.shared.open(url)
+        }
+    }
+}
